803548a75c6940ac98e8fa99033f41e314fd80da
Sebastian Barrozo <sebastian.barrozo@smfconsulting.es>
2022-09-20 11:37:51 -0300

    Hybrid storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../components/**/*.stories.?(ts|tsx|js|jsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-react-native-web",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+};
