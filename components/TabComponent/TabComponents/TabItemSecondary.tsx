b009c095d53f9dcf7e1f881ab0721d69fb398b88
Tomas Rodeghiero <tomas.rodeghiero@smfconsulting.es>
2022-09-26 13:02:47 -0300

    Feature EPL-417: TabItem Initial commit

---
+++ components/TabComponent/TabComponents/TabItemSecondary.tsx
@@ -0,0 +1,52 @@
+{/* Imports */}
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+{/* Type declaration */}
+type Info = {
+  name: string;
+  route: string;
+};
+
+interface Props {
+  style: any; 
+  item: any;
+  onPress: () => void;
+  index: number;
+  toggleItem: number;
+}
+
+{/* Styles */}
+const styles = StyleSheet.create({
+  itemText: {
+    fontFamily: 'Inter-SemiBold',
+    textTransform: 'uppercase',
+  }
+})
+
+{/* TabItem component */}
+const TabItem = ({style, item, index, onPress, toggleItem}: Props) => {
+  {/* TabItem text colors */}
+  const colorActive = '#202452';
+  const colorInactive = '#808695';
+
+  return (
+    <TouchableOpacity
+      style={style}
+      onPress={() => {
+        onPress();
+      }}
+      key={item.key}>
+      <Text
+        style={
+          toggleItem === index
+            ? {color: colorActive, ...styles.itemText}
+            : {color: colorInactive, ...styles.itemText}
+        }>
+        {item.name}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TabItem;
