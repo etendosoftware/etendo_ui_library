0b94a3195ae41dd4a51b7f9f56606801b608722e
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2022-11-07 16:41:38 -0300

    Feature EPL-421 Table sonar problem

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -19,6 +19,111 @@ import {
 } from '../../helpers/utilTable';
 
 const MovementTable = ({addedItems, config, passDataToParent}: TableProps) => {
+  const getIcon = (check: boolean) => {
+    if (check) {
+      return (
+        <Image
+          style={{height: 14, width: 14}}
+          source={require('../../assets/images/icons/checkTrue.png')}
+        />
+      );
+    } else {
+      return (
+        <Image
+          style={{height: 14, width: 14}}
+          source={require('../../assets/images/icons/checkFalse.png')}
+        />
+      );
+    }
+  };
+
+  const switchColumn = (col: any, item: any, index: number) => {
+    if (col.type === 'cellText') {
+      return (
+        <ScrollView
+          nestedScrollEnabled={true}
+          contentContainerStyle={styles.scrollCell}>
+          <Text style={styles.cellText}>{item[col.key]}</Text>
+        </ScrollView>
+      );
+    }
+    if (col.type === 'cellOnlyRead') {
+      return (
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.centerItem}
+          onPress={() => {
+            passDataToParent({
+              typeColumnPress: col.type,
+              key: col.key,
+              indexCurrent: index,
+            });
+          }}>
+          <Image
+            style={{height: 14, width: 11}}
+            source={require('../../assets/images/icons/document.png')}
+          />
+        </TouchableOpacity>
+      );
+    }
+    if (col.type === 'cellEdit') {
+      return (
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.centerItem}
+          onPress={() => {
+            passDataToParent({
+              typeColumnPress: col.type,
+              key: col.key,
+              indexCurrent: index,
+            });
+          }}>
+          <Image
+            style={[
+              {height: 13, width: 13},
+              item[col.key] ? {tintColor: GREEN} : {},
+            ]}
+            source={require('../../assets/images/icons/edit.png')}
+          />
+        </TouchableOpacity>
+      );
+    }
+    if (col.type === 'cellTextInput') {
+      return (
+        <Input
+          centerText={true}
+          value={item[col.key]}
+          typeField={'textPressable'}
+          onPress={() =>
+            passDataToParent({
+              typeColumnPress: col.type,
+              key: col.key,
+              indexCurrent: index,
+            })
+          }
+        />
+      );
+    }
+    if (col.type === 'cellCheck') {
+      return (
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.centerItem}
+          onPress={() => {
+            passDataToParent({
+              typeColumnPress: col.type,
+              key: col.key,
+              indexCurrent: index,
+            });
+          }}>
+          {(() => {
+            getIcon(item[col.key]);
+          })()}
+        </TouchableOpacity>
+      );
+    }
+  };
+
   const renderItem = (item: any, index: number, config: any) => {
     return (
       <View
@@ -32,106 +137,7 @@ const MovementTable = ({addedItems, config, passDataToParent}: TableProps) => {
                 removeLastBorderCell(colIndex, config.columns.length - 1),
               ]}
               key={'movementTable' + colIndex}>
-              {(() => {
-                if (col.type === 'cellText') {
-                  return (
-                    <ScrollView
-                      nestedScrollEnabled={true}
-                      contentContainerStyle={styles.scrollCell}>
-                      <Text style={styles.cellText}>{item[col.key]}</Text>
-                    </ScrollView>
-                  );
-                }
-                if (col.type === 'cellOnlyRead') {
-                  return (
-                    <TouchableOpacity
-                      activeOpacity={0.6}
-                      style={styles.centerItem}
-                      onPress={() => {
-                        passDataToParent({
-                          typeColumnPress: col.type,
-                          key: col.key,
-                          indexCurrent: index,
-                        });
-                      }}>
-                      <Image
-                        style={{height: 14, width: 11}}
-                        source={require('../../assets/images/icons/document.png')}
-                      />
-                    </TouchableOpacity>
-                  );
-                }
-                if (col.type === 'cellEdit') {
-                  return (
-                    <TouchableOpacity
-                      activeOpacity={0.6}
-                      style={styles.centerItem}
-                      onPress={() => {
-                        passDataToParent({
-                          typeColumnPress: col.type,
-                          key: col.key,
-                          indexCurrent: index,
-                        });
-                      }}>
-                      <Image
-                        style={[
-                          {height: 13, width: 13},
-                          item[col.key] ? {tintColor: GREEN} : {},
-                        ]}
-                        source={require('../../assets/images/icons/edit.png')}
-                      />
-                    </TouchableOpacity>
-                  );
-                }
-                if (col.type === 'cellTextInput') {
-                  return (
-                    <Input
-                      centerText={true}
-                      value={item[col.key]}
-                      typeField={'textPressable'}
-                      onPress={() =>
-                        passDataToParent({
-                          typeColumnPress: col.type,
-                          key: col.key,
-                          indexCurrent: index,
-                        })
-                      }
-                    />
-                  );
-                }
-                if (col.type === 'cellCheck') {
-                  return (
-                    <TouchableOpacity
-                      activeOpacity={0.6}
-                      style={styles.centerItem}
-                      onPress={() => {
-                        passDataToParent({
-                          typeColumnPress: col.type,
-                          key: col.key,
-                          indexCurrent: index,
-                        });
-                      }}>
-                      {(() => {
-                        if (item[col.key] === true) {
-                          return (
-                            <Image
-                              style={{height: 14, width: 14}}
-                              source={require('../../assets/images/icons/checkTrue.png')}
-                            />
-                          );
-                        } else {
-                          return (
-                            <Image
-                              style={{height: 14, width: 14}}
-                              source={require('../../assets/images/icons/checkFalse.png')}
-                            />
-                          );
-                        }
-                      })()}
-                    </TouchableOpacity>
-                  );
-                }
-              })()}
+              {switchColumn(col, item, index)}
             </View>
           );
         })}
