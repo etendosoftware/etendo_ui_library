954611fe477964715265f2047ef5563342766c83
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-02-09 15:24:09 -0300

    Feature EPL-517: add constant color

--- components/states/componentsStates/ComponentsStatesScreen.style.ts
+++ components/states/componentsStates/ComponentsStatesScreen.style.ts
@@ -1,19 +1,20 @@
 import {StyleSheet} from 'react-native';
+import {LIGHT_BLACK} from '../../../styles/colors';
 
 export const styles = StyleSheet.create({
   title: {
     fontSize: 30,
     fontWeight: '600',
     marginBottom: 8,
-    color: '#4a4f62',
+    color: LIGHT_BLACK,
   },
   description: {
     marginBottom: 20,
-    color: '#4a4f62',
+    color: LIGHT_BLACK,
   },
   lenghtComponents: {
     fontWeight: '600',
-    color: '#4a4f62',
+    color: LIGHT_BLACK,
     marginBottom: 20,
   },
 });
