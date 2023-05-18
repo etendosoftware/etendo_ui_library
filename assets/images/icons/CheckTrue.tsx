f2998b56ef697884e0298ecf8c54c01ecdf65d1c
joacocorti <joaquin.corti@smfconsulting.es>
2023-05-18 10:34:45 -0300

    Feature EPL-613: change clip and fill rules on svg

--- assets/images/icons/CheckTrue.tsx
+++ assets/images/icons/CheckTrue.tsx
@@ -26,8 +26,8 @@ export const CheckTrue = ({style, fill, stroke}: SvgImageProps) => {
         fill={fill ? fill : '#E5F7ED'}
       />
       <Path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M17.5671 8.78117C17.1418 8.4061 16.4524 8.40646 16.0272 8.78117L11.0571 13.8046L8.86323 11.5555C8.43826 11.1196 7.74886 11.1196 7.34421 11.5556C7.03736 11.9917 7.03736 12.6987 7.34421 13.1347L10.2867 16.1731C10.7121 16.609 11.4015 16.609 11.8268 16.1731L17.5671 10.2879C17.9921 9.85169 17.9921 9.14478 17.5671 8.78117Z"
         fill={fill ? fill : '#00B34C'}
       />
