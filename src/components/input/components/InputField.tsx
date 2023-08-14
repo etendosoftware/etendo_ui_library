e39a8029a53af317f426146f1bb6867cfe37f179
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-08-14 12:21:19 -0300

    Feature EPL-823: fix sonar comment

--- src/components/input/components/InputField.tsx
+++ src/components/input/components/InputField.tsx
@@ -1,10 +1,4 @@
-import React, {
-  ReactComponentElement,
-  ReactElement,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   TextInput,
   TextStyle,
