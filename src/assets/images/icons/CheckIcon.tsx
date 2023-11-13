ab2d15430051404e914fa50c132eccd5efdf4495
JoaquinCorti <joaquin.corti@etendo.software>
2023-11-13 12:45:43 -0300

    Feature EPL-1096: changes in false icon

--- src/assets/images/icons/CheckIcon.tsx
+++ src/assets/images/icons/CheckIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ImageStyle } from 'react-native';
 import { Svg, Path } from 'react-native-svg';
-import { DEFAULT_COLOR_THEME, NEUTRAL_0 } from '../../../styles/colors';
+import { DEFAULT_COLOR_THEME } from '../../../styles/colors';
 
 export interface SvgImageProps {
   style?: ImageStyle;
@@ -13,15 +13,17 @@ export const CheckIcon = ({
   fill = DEFAULT_COLOR_THEME,
 }: SvgImageProps) => {
   return (
-    <Svg x="0px" y="0px" viewBox="0 0 25 25" style={style}>
+    <Svg
+      width={style?.width ?? 43}
+      height={style?.height ?? 32}
+      viewBox="0 0 43 32"
+      fill="none"
+      style={style}>
       <Path
-        fill={NEUTRAL_0}
-        d="M5,0.8h15c2.3,0,4.2,1.9,4.2,4.2v15c0,2.3-1.9,4.2-4.2,4.2H5c-2.3,0-4.2-1.9-4.2-4.2V5C0.8,2.7,2.7,0.8,5,0.8z"
-      />
-      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M41.0309 1.12469C39.3624 -0.37562 36.6578 -0.374175 34.9899 1.12469L15.4917 21.2185L6.88504 12.2221C5.21785 10.4783 2.51325 10.4783 0.92578 12.2224C-0.277994 13.9669 -0.277994 16.7949 0.92578 18.5386L12.4693 30.6925C14.1382 32.4358 16.8428 32.4358 18.5114 30.6925L41.0309 7.15158C42.6985 5.40676 42.6985 2.57913 41.0309 1.12469Z"
         fill={fill}
-        d="M20,25H5c-2.8,0-5-2.2-5-5V5c0-2.8,2.2-5,5-5h15c2.8,0,5,2.2,5,5v15C25,22.8,22.8,25,20,25z M5,1.5
-	C3.1,1.5,1.5,3.1,1.5,5v15c0,1.9,1.6,3.5,3.5,3.5h15c1.9,0,3.5-1.6,3.5-3.5V5c0-1.9-1.6-3.5-3.5-3.5H5z"
       />
     </Svg>
   );
