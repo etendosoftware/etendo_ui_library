6f51d83568f96bdd5650df45836aaa9079868748
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-11-23 12:31:42 -0300

    Feature EPL-1113: Standardize example files with the same structure

--- examples/nextjs/apps/web/app/page.tsx
+++ examples/nextjs/apps/web/app/page.tsx
@@ -1,28 +1,12 @@
 'use client';
 
-import { useState } from 'react';
 import Button from './components/button/Button';
-import Input from './components/input/Input';
-import { CardDropdown } from './components/cards/cardDropdown';
 
 export default function Web() {
-  const [value, setValue] = useState('');
-
   return (
     <div>
       <h1>Etendo UI Library</h1>
       <Button typeStyle={'primary'} text="Button" />
-      <Input
-        value={value}
-        onChangeText={(text: string) => setValue(text)}
-        typeField={'textInput'}
-      />
-      <Input
-        value={value}
-        onChangeText={(text: string) => setValue(text)}
-        typeField={'pressableSearch'}
-      />
-      <CardDropdown />
     </div>
   );
 }
