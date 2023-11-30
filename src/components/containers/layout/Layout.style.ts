e710fe5d5338221e842eefe34a7a74e3c6c832fa
Leandro Allemandi <leandro.allemandi@smfconsulting.es>
2023-11-30 17:35:32 -0300

    Feature EPL-1132: add 100% in Layout dimensions

--- src/components/containers/layout/Layout.style.ts
+++ src/components/containers/layout/Layout.style.ts
@@ -5,6 +5,8 @@ export const defaultStyle: ViewStyle = {
   ...paddingVerticalContainer(),
   display: 'flex',
   flex: 1,
+  width: '100%',
+  height: '100%',
   justifyContent: 'flex-start',
   alignItems: 'center',
 };
