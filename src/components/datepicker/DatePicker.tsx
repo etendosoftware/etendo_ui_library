3dd72a8c63ee7b3fcd3e9be5b2bbfc8d08a05665
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-12-05 15:54:52 -0300

    Feature EPL-1127: Fix position Picker

--- src/components/datepicker/DatePicker.tsx
+++ src/components/datepicker/DatePicker.tsx
@@ -23,6 +23,7 @@ import {
 import {
   NEUTRAL_300,
   NEUTRAL_400,
+  NEUTRAL_50,
   NEUTRAL_600,
   PRIMARY_100,
 } from '../../styles/colors';
@@ -48,11 +49,11 @@ const DatePicker = ({
   value,
   styleField,
   onChangeText,
-  backgroundColor,
-  dateFormat = 'MM/DD/YYYY',
   language = 'en-US',
-  showCalendar = true,
+  dateFormat = 'MM/DD/YYYY',
   disabled = false,
+  showCalendar = true,
+  backgroundColor = NEUTRAL_50,
 }: DatePickerProps) => {
   // states for the date picker
   const [hoveredDay, setHoveredDay] = useState<any>(null);
@@ -72,6 +73,10 @@ const DatePicker = ({
   const [currentSelectedDate, setTempSelectedDate] = useState<Date>(
     new Date(selectedDate),
   );
+  const [modalPosition, setModalPosition] = useState<any>({
+    top: undefined,
+    bottom: undefined,
+  });
 
   // Effect to validate and set the selected date
   useEffect(() => {
@@ -286,11 +291,16 @@ const DatePicker = ({
         ) => {
           const windowHeight = Dimensions.get('window').height;
           const spaceBelow = windowHeight - pageY - height;
+          let modalLeftPosition = pageX + width - MODAL_CONTENT_WIDTH;
+          modalLeftPosition = Math.max(modalLeftPosition, 0);
 
-          if (spaceBelow < CALENDAR_HEIGHT) {
-            setCalendarDirection('upwards');
+          if (spaceBelow >= CALENDAR_HEIGHT) {
+            setModalPosition({ top: pageY + height, left: modalLeftPosition });
           } else {
-            setCalendarDirection('downwards');
+            setModalPosition({
+              top: pageY - CALENDAR_HEIGHT - 155,
+              left: modalLeftPosition,
+            });
           }
         },
       );
@@ -499,9 +509,6 @@ const DatePicker = ({
     );
   };
 
-  // Conditional component based on platform
-  const PlatformComponent: any = Platform.OS === AppPlatform.web ? View : Modal;
-
   return (
     <View style={styles.container}>
       <View
@@ -545,29 +552,24 @@ const DatePicker = ({
         </View>
       </View>
 
-      <PlatformComponent
-        {...(Platform.OS === AppPlatform.web
-          ? {
-              style: {
-                display: isPickerShow ? 'flex' : 'none',
-              },
-            }
-          : {
-              transparent: true,
-              visible: isPickerShow,
-              onRequestClose: showPicker,
-            })}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={isPickerShow}
+        onRequestClose={showPicker}>
         <TouchableOpacity
-          style={!(Platform.OS === AppPlatform.web) && styles.modalContainer}
+          style={styles.modalContainer}
           activeOpacity={1}
           onPress={showPicker}>
           <View
             style={[
               styles.modalContent,
               { width: MODAL_CONTENT_WIDTH },
-              calendarDirection === 'downwards'
-                ? null
-                : styles.modalContentUpwards,
+              {
+                position: 'absolute',
+                top: modalPosition.top + 5,
+                left: modalPosition.left,
+              },
             ]}
             onStartShouldSetResponder={() => true}>
             <View
@@ -654,7 +656,7 @@ const DatePicker = ({
             </View>
           </View>
         </TouchableOpacity>
-      </PlatformComponent>
+      </Modal>
     </View>
   );
 };
