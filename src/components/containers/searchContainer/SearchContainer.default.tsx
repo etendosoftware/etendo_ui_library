8c05fae7e8fbd1bae0d3e640b03c4b5a209ac381
Santiago Cuello <santiago.cuello@smfconsulting.es>
2023-12-14 09:17:26 -0300

    Feature EPL-1153: First approach

---
+++ src/components/containers/searchContainer/SearchContainer.default.tsx
@@ -0,0 +1,11 @@
+import { ViewStyle } from 'react-native';
+
+export const DEFAULT_SEARCH_CONTAINER: ViewStyle = {
+  paddingVertical: 16,
+  paddingHorizontal: 0,
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  gap: 12,
+  alignItems: 'center',
+  flexWrap: 'wrap',
+};
