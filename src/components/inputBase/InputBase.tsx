e379ccbb9011959384614b4a4269d2878ef5c020
Santiago Cuello <santiago.cuello@smfconsulting.es>
2024-01-22 08:08:37 -0300

    Feature EPL-1203: button container fixed

---
+++ src/components/inputBase/InputBase.tsx
@@ -0,0 +1,144 @@
+import {
+  ColorValue,
+  Text,
+  TextInput,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+import React, { useState } from 'react';
+import { IInputBase } from './InputBase.types';
+import { ButtonContainer } from '../containers';
+import { styles } from './InputBase.style';
+
+const InputBase = ({
+  value,
+  placeholder,
+  title,
+  helperText,
+  isDisabled,
+  isError,
+  onChangeText,
+  icon,
+  rightButtons,
+}: IInputBase) => {
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const onFocusChange = () => {
+    setIsFocused(true);
+  };
+
+  const onBlurChange = () => {
+    setIsFocused(false);
+  };
+
+  const borderWidth: number = isFocused ? 3 : 1;
+  const paddingVertical: number = 13 - (borderWidth - 1);
+  const paddingHorizontal: number = 12 - (borderWidth - 1);
+
+  const borderStyle = (): ViewStyle | undefined => {
+    if (!value && !isFocused) {
+      return styles.containerPlaceholder;
+    }
+    if (isDisabled) {
+      return styles.containerIsDisabled;
+    }
+    if (isError) {
+      return styles.containerIsError;
+    }
+  };
+
+  const iconColorStyle = (): ColorValue | undefined => {
+    if (!value) {
+      return styles.iconPlaceholder.color;
+    }
+    if (isDisabled) {
+      return styles.iconIsDisabled.color;
+    }
+    if (isFocused) {
+      return styles.iconIsFocus.color;
+    }
+    if (isError) {
+      return styles.iconIsError.color;
+    }
+  };
+
+  const textColorStyle = (): TextStyle | undefined => {
+    if (!value) {
+      return styles.textPlaceholder;
+    }
+    if (isDisabled) {
+      return styles.textIsDisabled;
+    }
+    if (isFocused) {
+      return styles.textIsFocus;
+    }
+    if (isError && !isFocused) {
+      return styles.textIsError;
+    }
+  };
+
+  return (
+    <>
+      {!!title && (
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.title}>
+          {title}
+        </Text>
+      )}
+      <View
+        style={[
+          styles.container,
+          borderStyle(),
+          {
+            borderWidth: borderWidth,
+            paddingVertical: paddingVertical,
+            paddingHorizontal: paddingHorizontal,
+          },
+        ]}>
+        {!!icon && (
+          <TouchableOpacity
+            disabled={!icon.onPress || isDisabled}
+            onPress={() => {
+              if (icon.onPress) {
+                icon.onPress();
+              }
+            }}
+            style={{ marginRight: 5 }}>
+            {React.cloneElement(icon.icon, {
+              style: icon.icon.props.style || styles.iconRight,
+              fill: iconColorStyle(),
+            })}
+          </TouchableOpacity>
+        )}
+        <TextInput
+          value={value}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          editable={!isDisabled}
+          onFocus={onFocusChange}
+          onBlur={onBlurChange}
+          style={[styles.textInput, textColorStyle()]}
+        />
+        {!!rightButtons && (
+          <ButtonContainer
+            style={{
+              flexDirection: 'row',
+              paddingVertical: 0,
+              gap: 0,
+              marginLeft: 5,
+            }}
+            buttons={rightButtons}
+          />
+        )}
+      </View>
+      {!!helperText && (
+        <Text numberOfLines={1} ellipsizeMode="tail" style={styles.helperText}>
+          {helperText}
+        </Text>
+      )}
+    </>
+  );
+};
+
+export default InputBase;
---
+++ src/components/inputBase/InputBase.types.ts
@@ -0,0 +1,18 @@
+import { ReactElement, ReactNode } from 'react';
+
+export interface IInputButtons {
+  icon: ReactElement;
+  onPress?: () => void;
+}
+
+export interface IInputBase {
+  value?: string;
+  placeholder?: string;
+  title?: string;
+  helperText?: string;
+  isDisabled?: boolean;
+  isError?: boolean;
+  onChangeText?: (value: string) => void;
+  icon?: IInputButtons;
+  rightButtons?: ReactNode[];
+}
