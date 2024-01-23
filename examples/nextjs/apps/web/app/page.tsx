b00f0d9de8d8a27f0b175adf53cf5ef63c7900a8
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2024-01-23 08:55:58 -0300

    Feature EPL-1209: Add examples files

---
+++ examples/nextjs/apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Button from './components/button/Button';
+
+export default function Web() {
+  return (
+    <div>
+      <h1>Etendo UI Library</h1>
+      <Button typeStyle={'primary'} text="Button" />
+    </div>
+  );
+}
