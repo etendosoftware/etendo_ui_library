cade0d2ff2af6cde66aa3c2a28a37624d2a86307
Santiago Cuello <santiago.cuello@smfconsulting.es>
2024-01-29 13:37:59 -0300

    Feature EPL-1224: add modal in index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,17 @@
 import { Button } from './button';
+import { CardDropdown } from './cards/components/cardDropdown';
 import { Input } from './input';
+import { Modal } from './modal';
 import { Navbar, DrawerLateral } from './navbar';
 import { Pagination } from './pagination';
+import { SkeletonItem } from './secondaryComponents';
 import { StatusBar } from './statusbar';
 import { Tab } from './tab';
 import { Table } from './table';
 import { ViewEtendo } from './viewEtendo/ViewEtendo';
-import { CardDropdown } from './cards/components/cardDropdown';
-import { SkeletonItem } from './secondaryComponents';
 import {
-  Layout,
   ButtonContainer,
+  Layout,
   SearchContainer,
   TitleContainer,
 } from './containers';
@@ -26,6 +27,7 @@ export {
   DrawerLateral,
   Input,
   Layout,
+  Modal,
   Navbar,
   Pagination,
   SearchContainer,
