ce4ae4933c49cc476498d08df6764d698c3df37d
Leandro <leandro.allemandi@smfconsulting.es>
2024-03-25 16:24:36 -0300

    Feature/EE-882: Rm buttonCotainer

--- src/components/containers/buttonContainer/ButtonContainer.types.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-import { StyleProp, ViewStyle } from 'react-native';
-
-export interface IButtonContainer {
-  buttons: ReactNode[];
-  style?: StyleProp<ViewStyle>;
-}
