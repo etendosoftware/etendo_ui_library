bcfe69431bd252855c0c06dda3d9028bf01210f0
Leandro <leandro.allemandi@smfconsulting.es>
2024-03-25 16:50:55 -0300

    Feature/EE-882: fix SearchContainer & TitleContainer

--- src/components/containers/searchContainer/SearchContainer.types.tsx
+++ src/components/containers/searchContainer/SearchContainer.types.tsx
@@ -1,12 +1,17 @@
-import { ReactNode } from 'react';
 import { DimensionValue, StyleProp, ViewStyle } from 'react-native';
+import { ISearchInput } from '../../inputBase/search-input/SearchInput.types';
+import { RightButtons } from '../../inputBase/InputBase.types';
 
-export interface ISearchContainer {
-  buttons: ReactNode[];
+export interface ISearchContainer extends Omit<ISearchInput, 'styleContainer'>{
+  buttons?: RightButtons;
   height?: number;
   onSubmit: (...params: any[]) => void;
   placeholder?: string;
   style?: StyleProp<ViewStyle>;
   value?: string;
   widthSearchInput?: DimensionValue;
+  styleContainer?: ViewStyle;
+  styleGridContainer?: ViewStyle;
+  gridGapHorizontal?: number;
+  gridGapVertical?: number;
 }
