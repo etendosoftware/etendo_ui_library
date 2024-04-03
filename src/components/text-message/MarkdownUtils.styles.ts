81a18370706eaf5a3a34e9fdfe6fed47450a3f7f
Leandro <leandro.allemandi@smfconsulting.es>
2024-04-03 16:01:36 -0300

    feature/EML-492: add hover in image textMessage & href

--- src/components/text-message/MarkdownUtils.styles.ts
+++ src/components/text-message/MarkdownUtils.styles.ts
@@ -1,47 +1,82 @@
 import { StyleSheet } from "react-native";
-import { INITIAL_100, NEUTRAL_0, NEUTRAL_800, PRIMARY_100, PRIMARY_20, TERTIARY_70 } from "../../styles/colors";
+import {
+  INITIAL_100,
+  NEUTRAL_0,
+  NEUTRAL_800,
+  PRIMARY_100,
+  PRIMARY_20,
+  TERTIARY_70,
+} from "../../styles/colors";
 
 export const styles = StyleSheet.create({
-    boldText: {
-        fontWeight: 'bold',
-    },
-    italicText: {
-        fontStyle: 'italic',
-    },
-    copyButton: {
-        flexDirection: "row",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        padding: 5,
-        borderTopRightRadius: 4,
-        borderTopLeftRadius: 4,
-        backgroundColor: PRIMARY_20,
-        gap: 5,
-    },
-    copyButtonText: {
-        color: NEUTRAL_0,
-        fontSize: 12,
-    },
-    linkText: {
-        color: INITIAL_100,
-        fontWeight: "600",
-        textDecorationLine: 'underline',
-    },
-    inlineCode: {
-        backgroundColor: TERTIARY_70,
-        paddingHorizontal: 4,
-        borderRadius: 4,
-        color: PRIMARY_100,
-        fontWeight: "700",
-    },
-    paragraph: {
-        margin: 0,
-        padding: 0,
-        lineHeight: 20,
-        color: NEUTRAL_800,
-        fontWeight: "500",
-    },
-    listItem: {
-        fontWeight: "700",
-    },
+  boldText: {
+    fontWeight: "bold",
+  },
+  italicText: {
+    fontStyle: "italic",
+  },
+  copyButton: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 5,
+    borderTopRightRadius: 4,
+    borderTopLeftRadius: 4,
+    backgroundColor: PRIMARY_20,
+    gap: 5,
+  },
+  copyButtonText: {
+    color: NEUTRAL_0,
+    fontSize: 12,
+  },
+  linkText: {
+    color: INITIAL_100,
+    fontWeight: "600",
+    textDecorationLine: "underline",
+  },
+  inlineCode: {
+    backgroundColor: TERTIARY_70,
+    paddingHorizontal: 4,
+    borderRadius: 4,
+    color: PRIMARY_100,
+    fontWeight: "700",
+  },
+  paragraph: {
+    margin: 0,
+    padding: 0,
+    lineHeight: 20,
+    color: NEUTRAL_800,
+    fontWeight: "500",
+  },
+  listItem: {
+    fontWeight: "700",
+  },
 });
+
+export const styleMarkdown: any = {
+  imgContainerLink: {
+    textDecoration: "none",
+  },
+  imgContainer: {
+    margin: 8,
+    transition: "background-color 0.3s ease",
+    border: "1px solid #E0E0E0",
+    borderRadius: 8,
+    padding: 4,
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  img: {
+    borderRadius: 4,
+  },
+  imgButtonContainer: {
+    transition: "opacity 0.3s ease, visibility 0.3s ease",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+  boxShadow: "0px 4px 10px 0px #4242421A",
+  boxShadowActive: "0px 4px 10px 0px #42424233",
+};
--- src/components/text-message/MarkdownUtils.tsx
+++ src/components/text-message/MarkdownUtils.tsx
@@ -1,36 +1,88 @@
-import React from 'react';
-import { Text, ScrollView } from 'react-native';
-import { TextMessageProps } from './TextMessage.types';
-import ReactMarkdown from 'react-markdown';
-import { CodeComponent } from './CodeComponent';
-import { AnchorComponent } from './AnchorComponent';
-import { getTextColorByType } from './TextMessage.utils';
-import { styles } from './MarkdownUtils.styles';
+import React, { useState } from "react";
+import { Text, ScrollView } from "react-native";
+import { TextMessageProps } from "./TextMessage.types";
+import ReactMarkdown from "react-markdown";
+import { CodeComponent } from "./CodeComponent";
+import { AnchorComponent } from "./AnchorComponent";
+import { getTextColorByType } from "./TextMessage.utils";
+import { styleMarkdown, styles } from "./MarkdownUtils.styles";
+import { Button } from "../button";
+import { PlusIcon } from "../../assets/images/icons";
+import { PRIMARY_100 } from "../../styles/colors";
 
 const InlineCode = ({ children }: any) => (
-    <Text style={styles.inlineCode}>{children}</Text>
+  <Text style={styles.inlineCode}>{children}</Text>
 );
 
 const Paragraph = ({ children, type, ...props }: any) => {
-    return (
-        <Text style={[styles.paragraph, { color: getTextColorByType(type) }, props.style]} {...props}>
-            {children}
-        </Text>
-    );
+  return (
+    <Text
+      style={[
+        styles.paragraph,
+        { color: getTextColorByType(type) },
+        props.style,
+      ]}
+      {...props}
+    >
+      {children}
+    </Text>
+  );
+};
+const CustomImageComponent = ({ src, alt, style }: any) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <a
+      href={src}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={styleMarkdown.imgContainerLink}
+    >
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{
+          ...styleMarkdown.imgContainer,
+          background: isHovered ? PRIMARY_100 : "transparent",
+          boxShadow: isHovered
+            ? styleMarkdown.boxShadowActive
+            : styleMarkdown.boxShadow,
+        }}
+      >
+        <img src={src} alt={alt} style={{ ...style, ...styleMarkdown.img }} />
+      </div>
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        style={{
+          ...styleMarkdown.imgButtonContainer,
+          opacity: isHovered ? 1 : 0,
+          visibility: isHovered ? "visible" : "hidden",
+        }}
+      >
+        <Button typeStyle="primary" iconLeft={<PlusIcon />} />
+      </div>
+    </a>
+  );
 };
 
 // Component to render markdown text
-export const RenderMarkdownText: React.FC<TextMessageProps> = ({ text, type = "left-user" }) => {
-    return (
-        <ScrollView horizontal={false} style={{ flex: 1 }}>
-            <ReactMarkdown components={{
-                pre: CodeComponent,
-                code: InlineCode,
-                a: AnchorComponent,
-                p: ({ node, ...props }) => <Paragraph {...props} type={type} />,
-            }} >
-                {text}
-            </ReactMarkdown>
-        </ScrollView>
-    );
+export const RenderMarkdownText: React.FC<TextMessageProps> = ({
+  text,
+  type = "left-user",
+}) => {
+  return (
+    <ScrollView horizontal={false} style={{ flex: 1 }}>
+      <ReactMarkdown
+        components={{
+          pre: CodeComponent,
+          code: InlineCode,
+          a: AnchorComponent,
+          p: ({ node, ...props }) => <Paragraph {...props} type={type} />,
+          img: CustomImageComponent,
+        }}
+      >
+        {text}
+      </ReactMarkdown>
+    </ScrollView>
+  );
 };
